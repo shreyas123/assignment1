2380a74bde1d4f8c3fd7b52947ac6372c041c89b
shreyasagarwal <shreyas.agarwal@artirix.com>
2013-10-07 18:37:19 +0530

    Parsed content to read links and input.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,9 +1,54 @@
+require 'nokogiri'
+
 class Parser
 
-
-
-	def self.parse(content)
+	def initialize(content, options = {})
 		@content = content
-		self
+		@host = options[:host] || 'http://localhost/'
+		@links = []
+		@inputs = []
+	end
+
+	def self.parse(content, options = {})
+		parser = Parser.new(content, options)
+		parser
+	end
+
+	def links
+		document.search("//a[@href]").each do |a|
+        	u = a['href']
+        	next if u.nil? or u.empty?
+        	u = check(u)
+        	@links << u unless u.nil?
+      	end
+      	@links.uniq
+	end
+
+	def inputs
+		document.search("//input").each do |input|
+			@inputs << input
+		end
+		@inputs.uniq
+	end
+
+	private 
+
+	def document
+		@document ||= Nokogiri::HTML(@content)
+	end
+
+	def check(link)
+		if link.start_with?("/") || link.start_with?("#")
+			url = URI.join(@host, link)
+		else
+			url = URI.parse(link)
+			url = URI.parse("http://#{link}") if url.scheme.nil?
+		end
+
+		if url.host == URI(@host).host && url.path != ""
+			url.to_s
+		else
+			nil
+		end
 	end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Parser do 
 	describe '#parse' do 
 		it 'should assigns the content body' do 
-		 	parser = Parser.parse('hi')
+		 	parser = Parser.parse('hi', { :host => 'http://www.example.com' })
 			expect(parser.instance_variable_get(:@content)).to eq 'hi'
 		end
 	end
@@ -11,7 +11,43 @@ describe Parser do
 	describe '#links' do 
 		it 'returns the links in the total site' do 
 			parser = Parser.parse("<head><a href='/index.html'></a></head>")
-			expect(parser.links).to eq ["/index.html"]
+			expect(parser.links.length).to eq 1
+		end
+
+		it 'retuns the uniq and only ones which points to current host' do 
+			parser = Parser.parse("<head>
+				<a href='/index.html'></a>
+				<a href='http://www.google.com/abcd.html'></a>
+				<a href='www.google.com/abcd123.html'></a>
+				<a href='#'></a>
+				</head>", {:host => 'http://www.abcd.com'}
+			)
+			expect(parser.links).to eq ["http://www.abcd.com/index.html"]
+		end
+
+		it 'returns uniq links present in the page' do 
+			parser = Parser.parse("<head>
+				<a href='/index.html'></a>
+				<a href='/index.html'></a>
+				<a href='http://www.google.com/abcd.html'></a>
+				<a href='www.google.com/abcd123.html'></a>
+				<a href='#'></a>
+				</head>", {:host => 'http://www.abcd.com'}
+			)
+			expect(parser.links).to eq ["http://www.abcd.com/index.html"]
+		end
+	end
+
+	describe '#inputs' do 
+		it 'returns the input in the page' do 
+			parser = Parser.parse("<head>
+				<input type='text'/>
+				<input type='radio'/>
+				<input type='button'/>
+				<input type='checkbox' />
+				input is a pain
+				</head>")
+			expect(parser.inputs.length).to eq 4
 		end
 	end
 end
